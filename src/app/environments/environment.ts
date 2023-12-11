c3e227083b06ed25024f1618eb0c898b68687dca
erc-boop <144221601+erc-boop@users.noreply.github.com>
2023-12-11 23:51:14 +0800

    lala?

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAo3sr0hQ1hSzVdS17ImvabniaMcSBazmE",
+        authDomain: "myfirebase-59e34.firebaseapp.com",
+        databaseURL: "https://myfirebase-59e34-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "myfirebase-59e34",
+        storageBucket: "myfirebase-59e34.appspot.com",
+        messagingSenderId: "113761456011",
+        appId: "1:113761456011:web:f71275dbb70bc9616ea98e",
+       
+    }
+  };
