8c2bcea52a0516ec58b11c5777ebce80fae87d80
erc-boop <144221601+erc-boop@users.noreply.github.com>
2023-12-12 00:43:30 +0800

    connection to fire base on progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,15 +49,14 @@ const routes: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyB6BVF4a8U13WQ8nrasWBPI9sHLZz72ugQ",
-      authDomain: "gutierrez-firebase.firebaseapp.com",
-      databaseURL: "https://gutierrez-firebase-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "gutierrez-firebase",
-      storageBucket: "gutierrez-firebase.appspot.com",
-      messagingSenderId: "485807502694",
-      appId: "1:485807502694:web:e5bdce9d310c11127c84a7"
+  apiKey: "AIzaSyB_PVQ5nM2vnl12iFFRze5MNmaBGkUgfI4",
+  authDomain: "final-baraquiel.firebaseapp.com",
+  projectId: "final-baraquiel",
+  storageBucket: "final-baraquiel.appspot.com",
+  messagingSenderId: "546776873126",
+  appId: "1:546776873126:web:f7b13fbc22b2602369dc67",
     }),
-    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
     production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyAo3sr0hQ1hSzVdS17ImvabniaMcSBazmE",
-        authDomain: "myfirebase-59e34.firebaseapp.com",
-        databaseURL: "https://myfirebase-59e34-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "myfirebase-59e34",
-        storageBucket: "myfirebase-59e34.appspot.com",
-        messagingSenderId: "113761456011",
-        appId: "1:113761456011:web:f71275dbb70bc9616ea98e",
+    firebase: {
+        apiKey: "AIzaSyB_PVQ5nM2vnl12iFFRze5MNmaBGkUgfI4",
+  authDomain: "final-baraquiel.firebaseapp.com",
+  projectId: "final-baraquiel",
+  storageBucket: "final-baraquiel.appspot.com",
+  messagingSenderId: "546776873126",
+  appId: "1:546776873126:web:f7b13fbc22b2602369dc67",
        
     }
   };
