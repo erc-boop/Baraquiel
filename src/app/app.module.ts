2d589c79ce18fea3bed811fd2e3b6dceec2c18b5
erc-boop <144221601+erc-boop@users.noreply.github.com>
2023-12-14 15:06:58 +0800

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,65 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './shared/auth.service';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { HeaderComponent } from './header/header.component';
-import { PostComponent } from './post/post.component';
-import { PostListComponent } from './post-list/post-list.component';
-import { PostEditComponent } from './post-edit/post-edit.component';
-import { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { AuthService } from './auth/auth.service';
-import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from './environments/environment';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-
-
-
-const routes: Routes = [
-  { path: '', redirectTo: 'post-list', pathMatch: 'full' },
-  { path: 'post-list', component: PostListComponent },
-  { path: 'post-add', component: PostEditComponent },
-  { path: 'authentication', component: AuthComponent },
-  { path: 'post-edit/:index', component: PostEditComponent },
-  { path: 'login', component: LoginComponent }, // Add this line
-  { path: 'register', component: RegisterComponent }, // Add this line
-]
+import { AngularFireModule } from '@angular/fire/compat'
+import { environment } from '../environments/environment';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
+import { VarifyEmailComponent } from './component/varify-email/varify-email.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FileuploadComponent } from './component/fileupload/fileupload.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
-    // HeaderComponent,
-    // PostComponent,
-    // PostListComponent,
-    // PostEditComponent,
     LoginComponent,
     RegisterComponent,
-  
-    
-
+    ForgotPasswordComponent,
+    VarifyEmailComponent,
+    FileuploadComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(routes),
-    ReactiveFormsModule,
-    HttpClientModule,
-    AngularFireModule.initializeApp({
-      apiKey: "AIzaSyB6BVF4a8U13WQ8nrasWBPI9sHLZz72ugQ",
-      authDomain: "gutierrez-firebase.firebaseapp.com",
-      databaseURL: "https://gutierrez-firebase-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "gutierrez-firebase",
-      storageBucket: "gutierrez-firebase.appspot.com",
-      messagingSenderId: "485807502694",
-      appId: "1:485807502694:web:e5bdce9d310c11127c84a7"
-    }),
+    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-  ],
-  providers: [AuthService],
+    FormsModule
+  ],  
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
