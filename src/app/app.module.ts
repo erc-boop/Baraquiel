6d02b01069b7b004311ac101dd0a0b3befa7c5a6
erc-boop <144221601+erc-boop@users.noreply.github.com>
2023-12-18 22:36:51 +0800

    qweqwe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { FormsModule } from '@angular/forms';
+import { environment } from './environments/environment';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './header/header.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostEditComponent } from './post-edit/post-edit.component';
-import { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { AuthService } from './auth/auth.service';
-import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from './environments/environment';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
-
 
 const routes: Routes = [
   { path: '', redirectTo: 'post-list', pathMatch: 'full' },
@@ -24,9 +21,9 @@ const routes: Routes = [
   { path: 'post-add', component: PostEditComponent },
   { path: 'authentication', component: AuthComponent },
   { path: 'post-edit/:index', component: PostEditComponent },
-  { path: 'login', component: LoginComponent }, // Add this line
-  { path: 'register', component: RegisterComponent }, // Add this line
-]
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   declarations: [
@@ -38,29 +35,16 @@ const routes: Routes = [
     PostEditComponent,
     LoginComponent,
     RegisterComponent,
-  
-    
-
   ],
   imports: [
-    BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes),
+    BrowserModule,
+    RouterModule.forRoot(routes), 
     ReactiveFormsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp({
-        apiKey: "AIzaSyArRROLwa3g-C9-q1Z5t9kmuk1Cni2g5us",
-        authDomain: "cc105-project.firebaseapp.com",
-        databaseURL: "https://cc105-project-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "cc105-project",
-        storageBucket: "cc105-project.appspot.com",
-        messagingSenderId: "669394565626",
-        appId: "1:669394565626:web:58488ce14eac0f2abc4c43",
-        measurementId: "G-3VF4KCTRQB"
-    }),
-    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [AuthService],
+  providers: [AuthComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,39 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
-  registerEmail: string | undefined;
-  registerPassword: string | undefined;
+export class RegisterComponent {
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router) { }
-
-  ngOnInit() {
-    this.authService.getAuthState().subscribe((user: any) => {
-      if (user) {
-        this.router.navigate(['/add-account']);
-      }
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      registerEmail: ['', [Validators.required, Validators.email]],
+      registerPassword: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   register() {
-    if (this.registerEmail && this.registerPassword) {
-      this.authService.register(this.registerEmail, this.registerPassword)
-        .then((result: any) => {
-          console.log('User registered');
-          console.log(result); // This will log the result of the registration
-          window.alert('Registered successfully'); // This will display the alert
-          this.registerEmail = '';
-          this.registerPassword = '';
-          this.router.navigate(['/login']); // navigate to add account page
-        }).catch((error: any) => {
-          console.error(error);
-        });
-    }
+    // Your registration logic here
+    console.log(this.registerForm.value);
   }
 }
